a91f9132d61a93c2edb0c19ffa6ff305870e991c
Chloe Rice <chloe.mi.rice@gmail.com>
2017-04-29 01:34:24 -0400

    feature - routing complete

---
+++ src/reducers/loadingReducer.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_POSTS,
+  CREATE_POST,
+  UPDATE_POST,
+  DELETE_POST,
+  RECEIVE_POSTS
+} from './constants'
+
+const loadingReducer = (state = false, action) => {
+  switch (action.type) {
+    case FETCH_POSTS: return true
+    case CREATE_POST: return true
+    case UPDATE_POST: return true
+    case DELETE_POST: return true
+    case RECEIVE_POSTS: return false
+    default: return state
+  }
+}
+
+export default loadingReducer
