3885261443fc93ed94bcc3fc12ea5f6f18e8bae1
Chloe Rice <chloe.mi.rice@gmail.com>
2017-04-29 02:30:57 -0400

    content - removed unnecessary quotes around about text

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,13 +7,13 @@ const About = props => (
       <h2>Look at these paragraphs about me</h2>
     </header>
     <p>
-      I'm a web developer specializing in Node.js and React.js. I'm currently looking for a collaborative, cross-functional, user-centered team to learn from and solve problems with in New York. <a href='Chloe_Rice.pdf' target='_blank'>This</a> is how I look on paper (blog + portfolio coming soon).
+      I'm a web developer specializing in Node.js and React.js. I'm currently looking for a collaborative, cross-functional, user-centered team to learn from and solve problems with in New York. <a href='Chloe_Rice.pdf' target='_blank'>This</a> is how I look on a single sheet of paper.
     </p>
     <p>
       I believe in a free and open web, growing food over lawns, 80's workout tapes over gyms, bikes over cars, and never wearing anything you can’t do a roundhouse kick in.
     </p>
     <p>
-      'When I’m not building the internet, I love creating whimsical landscapes, undercover unitards, and vegan deliciousness for the obscurely allergic. I’m also one of those crazy people that runs far in the wilderness for no good reason.'
+      When I’m not building the internet, I love creating whimsical landscapes, undercover unitards, and vegan deliciousness for the obscurely allergic. I’m also one of those crazy people that runs far in the wilderness for no good reason.
     </p>
     <p>
       I’m always on the lookout for side projects that matter and good conversation. Don’t hesitate to reach out!
