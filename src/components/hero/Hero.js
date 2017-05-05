ad261954220553388ce163405b3342b9828eaa86
Chloe Rice <chloe.mi.rice@gmail.com>
2017-05-05 18:24:14 -0400

    layout - mobile's looking good now

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,12 +3,18 @@ import { Row, Col, Image } from 'react-bootstrap'
 
 import meEyesForward from '../../img/me-eyes-forward.svg'
 import meEyesLooking from '../../img/me-eyes-looking.svg'
-import macBook from '../../img/macbook-focus.svg'
-import hippoTwirl from '../../img/twirl-focus.svg'
-// import fruit from '../../img/fruit-focus.svg'
-// import bike from '../../img/bike-focus.svg'
-// import sewingMachine from '../../img/sewing-machine-focus.svg'
-// import huskyRay from '../../img/ray-focus.svg'
+import heroIllustration from '../../img/macbook-center.svg'
+import mobileHeroIllustration from '../../img/mobile-hero.svg'
+
+const heroText = {
+  twirl: "When I'm not coding, I love...dancing with my hippo colored dog Twirl.",
+  ray: "When I'm not coding, I love...running trails with my husky Ray.",
+  bike: "When I'm not coding, I love...riding my bike everywhere--near and far.",
+  fruit: "When I'm not coding, I love...eating, growing and hunting for fruit.",
+  sewing: "When I'm not coding, I love...making clothes (ask me about unitards!).",
+  macbook: "I'm a full stack web developer, specialized in building delightful user experiences for web and mobile with React."
+}
+
 import './Hero.css'
 
 export default class Hero extends Component {
@@ -16,55 +22,69 @@ export default class Hero extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      hovering: false,
-      thing: ''
+      hovering: false
     }
   }
 
   chloeLookRight = thing => event => {
-    this.setState({thing: thing, hovering: true})
+    this.setState({hovering: true})
   }
 
   chloeLookForward = thing => event => {
-    this.setState({thing: thing, hovering: false})
+    this.setState({hovering: false})
   }
 
   render() {
-  
+
     return (
       <Row id='Home'>
         <Col xs={12} sm={12} md={12} lg={12}>
           <Row>
-            <Col xs={6} sm={6} md={6} lg={6}>
+            <Col className='mobile-Hero-illustration' xs={12} sm={12} mdHidden lgHidden>
+              <Image
+                  onMouseEnter={this.chloeLookRight('macbook')}
+                  onMouseLeave={this.chloeLookForward('macbook')}
+                  onTouchStart={this.chloeLookRight('macbook')}
+                  onTouchEnd={this.chloeLookForward('macbook')}
+                  responsive
+                  src={mobileHeroIllustration}
+                  alt='An interactive illustration of some of Chloe`s favorite things: her dogs Twirl and Ray, her sewing machine, bike, MacBook, and an assortment of fruit.'
+                />
+                <h2 className='mobile-Hero-text'>
+                  {heroText.macbook}
+                </h2>
+            </Col>
+
+            <Col className='Hero-illustration' xsHidden smHidden md={6} lg={6}>
               {
                 this.state.hovering
                 ? <Image
-                    className='me-eyes-looking Hero-illustration'
+                    className='me-eyes-looking'
                     responsive
                     src={meEyesLooking}
                     alt='An illustrated self-portrait of Chloé.'
                   />
                 : <Image
-                    className='me-eyes-forward Hero-illustration'
+                    className='me-eyes-forward'
                     responsive
                     src={meEyesForward}
                     alt='An illustrated self-portrait of Chloé.'
                   />
               }
             </Col>
-            <Col xs={6} sm={6} md={6} lg={6}>
+            <Col className='Hero-illustration' xsHidden smHidden md={6} lg={6}>
               <Image
                   onMouseEnter={this.chloeLookRight('macbook')}
                   onMouseLeave={this.chloeLookForward('macbook')}
                   onTouchStart={this.chloeLookRight('macbook')}
                   onTouchEnd={this.chloeLookForward('macbook')}
-                  className='macbook Hero-illustration'
+                  className='Hero-illustration'
                   responsive
-                  src={macBook}
-                  alt='An illustrated Macbook Pro with text editor and terminal on screen.'
+                  src={heroIllustration}
+                  alt='An interactive illustration of some of Chloe`s favorite things: her dogs Twirl and Ray, her sewing machine, bike, MacBook, and an assortment of fruit.'
                 />
                 <h2 className='Hero-text'>
-                  I'm a full stack web developer, specializing in building delightful user experiences for web and mobile with React.
+                  {heroText.macbook}
                 </h2>
             </Col>
           </Row>
